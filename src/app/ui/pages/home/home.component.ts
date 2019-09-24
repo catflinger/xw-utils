26a4fa854f8998fd972d3c2ca50cb6d873becad6
Paul Drury <paul@drurys.org>
2019-09-24 10:20:27 +0100

    added welcome text to home page

--- src/app/ui/pages/home/home.component.ts
+++ src/app/ui/pages/home/home.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { PuzzleInfo } from 'src/app/model/puzzle-info';
 import { AppService, AppStatus, EditorType } from 'src/app/services/app.service';
 import { Subscription } from 'rxjs';
+import { AuthService, Credentials } from 'src/app/services/auth.service';
 
 @Component({
     selector: 'app-home',
@@ -14,15 +15,18 @@ export class HomeComponent implements OnInit, OnDestroy {
     public puzzleList: PuzzleInfo[] = [];
     private subs: Subscription[] = [];
     public appStatus: AppStatus;
+    public credentials: Credentials;
 
     constructor(
         private appService: AppService,
         private router: Router,
         private puzzleManagement: IPuzzleManager,
+        private authService: AuthService,
     ) { }
 
     ngOnInit() {
         this.subs.push(this.appService.getObservable().subscribe(appStatus => this.appStatus = appStatus));
+        this.subs.push(this.authService.observe().subscribe(credentials => this.credentials = credentials));
 
         this.subs.push(this.puzzleManagement.getPuzzleList().subscribe(
             (list) => this.puzzleList = list,
